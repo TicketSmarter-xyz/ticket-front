08725499e7619caa9ace71292cf4e77e822df03d
ArturVargas <arturvargas9226@gmail.com>
2022-08-21 00:51:25 -0500

    add profile page

--- src/components/signup-walletconnect/signup-walletconnect.component.jsx
+++ src/components/signup-walletconnect/signup-walletconnect.component.jsx
@@ -2,7 +2,6 @@
 import WalletConnect from "@walletconnect/client";
 import { convertUtf8ToHex } from "@walletconnect/utils";
 import { useEffect, useState } from "react";
-import { recoverPersonalSignature } from "eth-sig-util";
 import WCQrMordal from "@walletconnect/qrcode-modal";
 import QRCode from "qrcode.react";
 import axios from "axios";
